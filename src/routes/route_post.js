2534cd1f0f1e7f55954253e452703cde367087ef
CupOfCoffe <edwinjsa2021@gmail.com>
2025-09-27 02:04:30 -0600

    :bug: solucionando el problema del multer que no acepta archivos grandes

--- src/routes/route_post.js
+++ src/routes/route_post.js
@@ -3,7 +3,11 @@ import * as module from "../controllers/controller_post.js";
 import multer from "multer";
 
 const router = Router();
-const upload = multer();
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 300 * 1024 * 1024 } // limite 300 MB
+});
+
 
 // Ruta para crear un nuevo post
 router.post("/create", upload.single("archivo"), module.crearPost);
